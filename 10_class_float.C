2db653ebd33910c9ca981e697a9f001b7611ba18
varshaGhosh <dalipghosh22@gmail.com>
2026-07-16 18:53:46 +0530

    Create 10_class_float.C

---
+++ 10_class_float.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main (void)
+
+{
+    float weight_1 = 12.4679;
+    printf("weight %f\n",weight_1);
+    
+    
+    float Rupees = 50.35;
+    printf("Rupeesvarsha %f\n",Rupees);
+    
+    
+    float Bank_Blance = 5864.2357078;
+    printf("Bank Blance %f\n", Bank_Blance);
+
+    
+
+}
