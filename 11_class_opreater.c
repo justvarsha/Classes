23d7e10f14182fde4297892b0646f95e5ed9af12
varshaGhosh <dalipghosh22@gmail.com>
2026-07-16 18:57:10 +0530

    Create 11_class_opreater.c

---
+++ 11_class_opreater.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1 = 4;
+    int num2 = 6;
+
+    int sum = num1+num2;
+    printf("the sum is:%d\n",sum);
+    // printf("the sum is:%d\n",num1+num2);
+
+    int difference = num2 - num1;
+    printf("the difference_1 is:%d\n", difference);
+    // printf("the difference_1 is:%d\n",num2 - num1);
+
+
+    int difference_2 = num1 - num2;
+    printf("the difference_2 is:%d\n",difference_2);
+    //printf("the difference_2 is:%d\n",num1 - num2);
+}
